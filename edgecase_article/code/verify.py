09b1eb6d208a840e7d0bd4a05359a19dd863d1c5
StJohn Piano <stjohnpiano@gmail.com>
2021-04-27 18:38:45 +0100

    Expand verify.py to verify a SignedArticle's signature

--- edgecase_article/code/verify.py
+++ edgecase_article/code/verify.py
@@ -10,16 +10,14 @@ import pkgutil
 from .. import util
 from .. import submodules
 from . import Article
+from . import SignedArticle
 
 
 
 
 # Shortcuts
-isfile = os.path.isfile
 v = util.validate
 datajack = submodules.datajack
-stateless_gpg = submodules.stateless_gpg
-gpg = stateless_gpg.gpg
 
 
 
@@ -60,6 +58,7 @@ def verify(
     article_type = None,
     verify_file_name = None,
     verify_signature = None,
+    public_key_dir = None,
     ):
   v.validate_string(article_path)
   v.validate_string(article_type, 'article_type', 'verify.py')
@@ -67,9 +66,6 @@ def verify(
   v.validate_boolean(verify_signature)
   if article_type != 'unspecified':
     v.validate_article_type(article_type)
-  if not isfile(article_path):
-    msg = "File not found at article_path {}".format(repr(article_path))
-    raise ValueError(msg)
   e = datajack.Element.from_file(article_path)
   msg = "File {} contains a valid Element.".format(article_path)
   log(msg)
@@ -81,14 +77,42 @@ def verify(
         repr(article_type), repr(e.name),
       )
       raise ValueError(msg)
-  msg = "Element name: {}.".format(e.name)
+  msg = "Element name: {}".format(e.name)
   log(msg)
   # Use the element name to select an article class.
+  a = None
   if article_type == 'article':
-    article = Article.Article.from_element(element=e)
-    article.set_file_path(article_path)
-    if verify_file_name:
-      article.validate_file_name()
+    a = Article.Article.from_element(element=e)
+  elif article_type == 'signed_article':
+    a = SignedArticle.SignedArticle.from_element(element=e)
   else:
     msg = "No class available for article_type {}".format(repr(article_type))
     raise ValueError(msg)
+  a.set_file_path(article_path)
+  if verify_file_name:
+    a.validate_file_name()
+  if verify_signature:
+    if a.article_type == 'signed_article':
+      author_name = a.author_name
+      public_key = load_public_key(public_key_dir, author_name)
+      a.verify_signature(public_key)
+    msg = "Signature verified for {}".format(a.__class__.__name__)
+    log(msg)
+
+
+
+
+def load_public_key(public_key_dir, author_name):
+  # Example public key file name:
+  # stjohn_piano_public_key.txt
+  pk_ext = '_public_key.txt'
+  items = os.listdir(public_key_dir)
+  key_file_names = [x for x in items if os.path.splitext(x)[1] == '.txt']
+  author_names = [x.replace(pk_ext, '') for x in key_file_names]
+  if author_name not in author_names:
+    msg = "Did not find any public key with author name {} in public key directory {}.".format(repr(author_name), repr(public_key_dir))
+    raise FileNotFoundError(msg)
+  key_file_name = author_name + pk_ext
+  key_file = os.path.join(public_key_dir, key_file_name)
+  public_key = open(key_file).read().strip()
+  return public_key
