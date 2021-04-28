b6de01db51aaf92997f190c3b7d6a819aa017176
StJohn Piano <stjohnpiano@gmail.com>
2021-04-28 14:58:11 +0100

    CheckpointArticle.py

---
+++ edgecase_article/code/CheckpointArticle.py
@@ -0,0 +1,137 @@
+# Imports
+import os
+import logging
+import pkgutil
+
+
+
+
+# Relative imports
+from .. import util
+from .. import submodules
+
+
+
+
+# Shortcuts
+v = util.validate
+datajack = submodules.datajack
+
+
+
+
+# Set up logger for this module. By default, it produces no output.
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
+logger.setLevel(logging.ERROR)
+log = logger.info
+deb = logger.debug
+
+
+
+
+def setup(
+    log_level = 'error',
+    debug = False,
+    log_timestamp = False,
+    log_filepath = None,
+    ):
+  # Configure logger for this module.
+  util.module_logger.configure_module_logger(
+    logger = logger,
+    logger_name = __name__,
+    log_level = log_level,
+    debug = debug,
+    log_timestamp = log_timestamp,
+    log_filepath = log_filepath,
+  )
+  log('Setup complete.')
+  deb('Logger is logging at debug level.')
+
+
+
+
+class CheckpointArticle(datajack.Element):
+
+
+  @classmethod
+  def from_element(cls, element):
+    e = element
+    e.__class__ = CheckpointArticle
+    e.validate_format()
+    e.article_type = 'checkpoint_article'
+    e.file_path = None
+    e.file_name = None
+    return e
+
+
+  def __str__(self):
+    return 'CheckpointArticle: [{t}]'.format(t=self.title)
+
+
+  @property
+  def checkpoint_id(self):
+    return self.get_value('checkpoint_id')
+
+
+  @property
+  def title(self):
+    return self.get_value('title')
+
+
+  @property
+  def uri_title(self):
+    return util.misc.uri_title(self.title)
+
+
+  @property
+  def date(self):
+    return self.get_branch_value('date').strip()
+
+
+  @property
+  def blockchain_name(self):
+    return self.get_value('block/blockchain_name')
+
+
+  @property
+  def block_height(self):
+    return self.get_branch_value('block/block_height')
+
+
+  def validate_format(self):
+    names = sorted(self.element_children_names)
+    expected = 'checkpoint_id title date block content'
+    expected = sorted(expected.split())
+    if names != expected:
+      msg = "Found these child elements: "
+      msg += ''.join(['\n-' + x for x in names])
+      msg += '\nBut expected these child elements:'
+      msg += ''.join(['\n-' + x for x in expected])
+      raise ValueError(msg)
+    v.validate_string_is_whole_number(self.checkpoint_id)
+    v.validate_title(self.title, article_type='checkpoint_article')
+    v.validate_date(self.date)
+    names2 = sorted(self.get_one('block').element_children_names)
+    expected2 = 'blockchain_name block_height'
+    expected2 = sorted(expected2.split())
+    if names2 != expected2:
+      msg = "For child 'block', found these child elements: "
+      msg += ''.join(['\n-' + x for x in names2])
+      msg += '\nBut expected these child elements:'
+      msg += ''.join(['\n-' + x for x in expected2])
+      raise ValueError(msg)
+
+
+  def set_file_path(self, file_path):
+    v.validate_string(file_path)
+    self.file_path = file_path
+    file_name = os.path.basename(file_path)
+    self.file_name = file_name
+
+
+  def validate_file_name(self):
+    v.validate_checkpoint_article_file_name(
+      self.file_name,
+      self.uri_title,
+    )
