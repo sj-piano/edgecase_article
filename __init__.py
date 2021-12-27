6fbc7545bde29d66aebca88c71e33990686f95a1
StJohn Piano <stjohnpiano@gmail.com>
2021-12-27 18:44:55 +0100

    Add shortcut for using 'keys' from a parent repo

--- __init__.py
+++ __init__.py
@@ -24,6 +24,7 @@ hello = edgecase_article.code.hello.hello
 validate = edgecase_article.util.validate
 configure_module_logger = edgecase_article.util.module_logger.configure_module_logger
 #submodules = edgecase_article.submodules
+keys = edgecase_article.code.keys
 verify = edgecase_article.code.verify.verify
 uri_title = edgecase_article.util.misc.uri_title
 
